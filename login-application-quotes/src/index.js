33da614faf66ec04ce3bac2a31bff14932932fc4
Eliyathamby Ajanthan | Consultant Yenlo <eliyathamby.ajanthan@acwapower.com>
2024-03-31 22:12:20 +0200

    Redirect URL update

--- login-application-quotes/src/index.js
+++ login-application-quotes/src/index.js
@@ -4,8 +4,8 @@ import { AuthProvider } from "@asgardeo/auth-react";
 import App from "./App";
 
 const config = {
-    signInRedirectURL: "http://localhost:3000",
-    signOutRedirectURL: "http://localhost:3000",
+    signInRedirectURL: "https://40b46b8d-dafd-44e0-8767-95a190272997.e1-eu-north-azure.choreoapps.dev",
+    signOutRedirectURL: "https://40b46b8d-dafd-44e0-8767-95a190272997.e1-eu-north-azure.choreoapps.dev",
     clientID: "QCdEtLbi82BHPzXa_8Jfw6EMiGwa",
     baseUrl: "https://api.asgardeo.io/t/ajanthanorg",
     scope: ["openid", "profile"]
