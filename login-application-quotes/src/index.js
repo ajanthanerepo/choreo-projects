ef2b74ec0b899942d7a005a3c1463bdf44ec969b
Eliyathamby Ajanthan | Consultant Yenlo <eliyathamby.ajanthan@acwapower.com>
2024-04-01 12:24:08 +0200

    Prod Config

--- login-application-quotes/src/index.js
+++ login-application-quotes/src/index.js
@@ -4,8 +4,8 @@ import { AuthProvider } from "@asgardeo/auth-react";
 import App from "./App";
 
 const config = {
-    signInRedirectURL: "https://40b46b8d-dafd-44e0-8767-95a190272997.e1-eu-north-azure.choreoapps.dev",
-    signOutRedirectURL: "https://40b46b8d-dafd-44e0-8767-95a190272997.e1-eu-north-azure.choreoapps.dev",
+    signInRedirectURL: "https://615e45c5-185f-467c-8118-16efaadc0236.e1-eu-north-azure.choreoapps.dev",
+    signOutRedirectURL: "https://615e45c5-185f-467c-8118-16efaadc0236.e1-eu-north-azure.choreoapps.dev",
     clientID: "QCdEtLbi82BHPzXa_8Jfw6EMiGwa",
     baseUrl: "https://api.asgardeo.io/t/ajanthanorg",
     scope: ["openid", "profile"]
